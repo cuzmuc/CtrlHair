5b93b08f583f0585b1757325053367de4cfbd4d7
cuzmuc <135721933+cuzmuc@users.noreply.github.com>
2023-09-22 09:23:05 +0300

    Update download.sh

---
+++ script/download.sh
@@ -0,0 +1,4 @@
+gdown https://drive.google.com/drive/folders/15uNXeRBIhVvZJIhL4yTw4IsStMhUaaxl -O ./external_model_params --folder
+gdown https://drive.google.com/drive/folders/1opQhmc7ckS3J8qdLii_EMqmxYCcBLznO -O ./model_trained --folder
+gdown https://drive.google.com/file/d/17iRY9zkQk7_qq2MdAyJBE54nlEHzA04R/view?usp=drive_link -O ./dataset_info_ctrlhair/hsv_stat_dict_ordered.pkl
+
