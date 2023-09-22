253daaf4ff2d6997980c2c2c7f9f845998a6e2a5
cuzmuc <135721933+cuzmuc@users.noreply.github.com>
2023-09-22 09:18:24 +0300

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -1,5 +1,5 @@
 pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
-pip install pandas scikit-learn opencv-python tqdm addict dill dlib
+pip install gdown pandas scikit-learn opencv-python tqdm addict dill dlib
 
 pip install PyQt6==6.4.2
 pip install PyQt6-Qt6==6.4.3
